ae8b5e8d7329e308b61c1beb61df67fe7eab38bf
leonid_shuliak <zeleny777@gmail.com>
2023-08-17 10:38:32 +0300

    Initial commit

---
+++ apps/catalog/src/app.module.ts
@@ -0,0 +1,14 @@
+import {MiddlewareConsumer, Module} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply().forRoutes('*');
+  }
+}
